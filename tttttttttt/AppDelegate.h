2b67610de715afefbc61521b6a50be09095a84c8
Cansing <Cansing@zhangjiandeMacBook-Pro.local>
2015-02-06 01:05:58 +0800

    Initial Commit

---
+++ tttttttttt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tttttttttt
+//
+//  Created by Cansing on 15/2/6.
+//  Copyright (c) 2015年 Cansing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tttttttttt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tttttttttt
+//
+//  Created by Cansing on 15/2/6.
+//  Copyright (c) 2015年 Cansing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
